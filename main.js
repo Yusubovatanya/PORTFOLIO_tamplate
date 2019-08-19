dd660c765afff16b82327c41d837fbaa5fbad788
Tanya <tanyagotovets@gmail.com>
2019-08-19 18:18:22 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: !true,
+  dotsSpeed: 2000,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 1
+    },
+    1000: {
+      items: 1
+    }
+  }
+})
